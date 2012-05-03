6af60e4dd74ddf00bf3e53b400b51060a5deeb64
Schalk Neethling <sneethling@mozilla.com>
2012-05-03 14:45:04 -0700

    stuff

--- static/rpsls.js
+++ static/rpsls.js
@@ -3,4 +3,19 @@ $(function() {
         event.preventDefault();
         $(".register").toggle();
     });
+    
+    $(".icon").click(function(event) {
+        event.preventDefault();
+        var currentIcon = $(this).find('img'),
+        currentIconClass = $(this).find('img').attr('class');
+        
+        $(".play-icons img").not("." + currentIconClass).hide();
+        currentIcon.animate({
+            left: '65',
+            top: '100',
+            height: '250',
+            width: '250'
+          }, 500);
+          currentIcon.parents('li').append('<p class="icon_name">' + currentIconClass + '</p>');
+    });
 })
