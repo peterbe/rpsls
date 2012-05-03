538e103bf92fff206031a3aa94a2a5aeace2a26a
Peter Bengtsson <mail@peterbe.com>
2012-05-03 11:21:13 -0700

    stuff

---
+++ sock_app.py
@@ -0,0 +1,43 @@
+import datetime
+import tornado.options
+import tornado.escape
+from tornado import web, ioloop
+from sockjs.tornado import SockJSRouter, SockJSConnection
+from tornado.options import define, options
+
+define("debug", default=False, help="run in debug mode", type=bool)
+define("port", default=9999, help="run on the given port", type=int)
+
+
+class PlayConnection(SockJSConnection):
+    _connected = set()
+
+    def on_open(self, *args, **kwargs):
+        #print "ARGS", args
+        #print "KWARGS", kwargs
+        self._connected.add(self)
+
+    def on_message(self, msg):
+        try:
+            data = tornado.escape.json_decode(msg)
+        except ValueError:
+            data = msg
+        print "DATA", repr(data)
+        data['date'] = datetime.datetime.now().strftime('%H:%M')
+        self.broadcast(self._connected, data)
+
+    def on_close(self):
+        # Remove client from the clients list and broadcast leave message
+        self._connected.remove(self)
+        self.broadcast(self.participants, "Someone left.")
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    EchoRouter = SockJSRouter(PlayConnection, '/play')
+    app_settings = dict(
+      debug=options.debug
+    )
+    app = web.Application(EchoRouter.urls, **app_settings)
+    app.listen(options.port)
+    print "Running sock app on port", options.port
+    ioloop.IOLoop.instance().start()
--- static/play.js
+++ static/play.js
@@ -1,3 +1,63 @@
+var Chat = (function() {
+  var _socket;
+
+  function init(socket) {
+    _socket = socket;
+    $('form').submit(function() {
+      var name = $.trim($('input[name="name"]').val());
+      var message = $.trim($('input[name="message"]').val());
+      if (!name.length) {
+        alert("No name :(");
+      } else if (!message.length) {
+        $('input[name="message"]').focus();
+      } else {
+        _socket.send_json({name: name, message: message});
+        $('input[name="message"]').val('');
+        $('input[name="message"]').focus();
+      }
+      return false;
+    });
+  }
+  return {init: init};
+
+})();
+
+SockJS.prototype.send_json = function(data) {
+  this.send(JSON.stringify(data));
+};
+
+var initsock = function(callback) {
+  sock = new SockJS('http://' + location.hostname + ':9999/play');
+
+  sock.onmessage = function(e) {
+    console.log('message', e.data);
+    $('<p>')
+      .text(e.data.date)
+        .css('float', 'right')
+          .appendTo($('#log'));
+    $('<p>')
+      .append($('<strong>').text(e.data.name + ': '))
+        .append($('<span>').text(e.data.message))
+          .appendTo($('#log'));
+  };
+  sock.onclose = function() {
+    console.log('closed');
+  };
+  sock.onopen = function() {
+    //log('opened');
+    console.log('open');
+    //sock.send('test');
+    if (sock.readyState !== SockJS.OPEN) {
+      throw "Connection NOT open";
+    }
+    callback(sock);
+  };
+};
+
+
 $(function() {
   console.log('let the madness begin!');
+  initsock(function(socket) {
+    Chat.init(socket);
+  });
 });
