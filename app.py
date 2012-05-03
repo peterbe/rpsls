617f050b76550a65cd53e587661ad9ee1b9f8efb
Peter Bengtsson <mail@peterbe.com>
2012-05-03 10:50:44 -0700

    more moves

--- app.py
+++ app.py
@@ -10,24 +10,30 @@ define("port", default=8000, help="run on the given port", type=int)
 
 class HomeHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("""<html>
-        Play more!
-        </html>""")
+        self.render('index.html')
+
+
+class ManifestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.set_header('Content-Type', 'application/x-web-app-manifest+json')
+        self.render('manifest.json')
+
 
 def app():
     app_settings = dict(
-      static_path=os.path.join(os.path.dirname(__file__), "static"),
-      template_path=os.path.join(os.path.dirname(__file__), "templates"),
+      static_path=os.path.join(os.path.dirname(__file__), 'static'),
+      template_path=os.path.join(os.path.dirname(__file__), 'templates'),
       debug=options.debug,
 
     )
     return tornado.web.Application([
-        (r"/", HomeHandler),
+        (r'/', HomeHandler),
+        (r'/manifest.webapp', ManifestHandler),
     ], **app_settings)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     tornado.options.parse_command_line()
     app().listen(options.port)
-    print "Running on port", options.port
+    print 'Running on port', options.port
     tornado.ioloop.IOLoop.instance().start()
